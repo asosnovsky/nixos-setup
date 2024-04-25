74e0af7dc6bfa1b991c720a01cde504e1dbf4061
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-04-25 11:14:00 -0400

    power management

--- modules/os/services.nix
+++ modules/os/services.nix
@@ -10,17 +10,45 @@
   };
 
   # Advance Power Management
+  powerManagement.powertop.enable = true;
+  powerManagement.enable = true;
+  services.thermald.enable = true;
   services.power-profiles-daemon.enable = false;
   services.tlp = {
     enable = true;
     settings = {
+      TLP_DEFAULT_MODE = "BAT";
+      TLP_PERSISTENT_DEFAULT = 1;
+
       CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
-      START_CHARGE_THRESH_BAT0 = 90;
-      STOP_CHARGE_THRESH_BAT0 = 97;
       RUNTIME_PM_ON_BAT = "auto";
       PLATFORM_PROFILE_ON_AC = "balanced";
       PLATFORM_PROFILE_ON_BAT = "low-power";
+
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      START_CHARGE_THRESH_BAT0 = 40; # and bellow it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 97; # and above it stops charging
+    };
+  };
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
     };
   };
 
@@ -36,6 +64,11 @@
   systemd.sleep.extraConfig = "HibernateDelaySec=30m";
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  # enables support for Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
 
   # Yubikey
   services.yubikey-agent.enable = true;
