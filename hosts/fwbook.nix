dbadcdd0655ab829a1f8ea2dfffe1e9e67fbe3e4
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-04-27 13:20:51 -0400

    trying flakes

--- hosts/fwbook.nix
+++ hosts/fwbook.nix
@@ -7,7 +7,6 @@
     [
       ./fwbook.hardware-configuration.nix
     ];
-  hardware.framework.amd-7040.preventWakeOnAC = true;
   services.fwupd.enable = true;
   fileSystems."${dataDir}" = {
     device = "/dev/sda1";
