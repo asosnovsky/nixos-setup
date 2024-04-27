4bb7f6760250f63f1f032320042e5fce263d6b07
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-04-27 13:26:28 -0400

    trying flakes

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
           #   hostName = "fwbook";
           #   user = user;
           # })
-          # ./modules/optional/amd-packages.nix
+          ./modules/optional/amd-packages.nix
           # ./modules/optional/gnome.nix
           # (import ./modules/optional/hyprland.nix {
           #   user = user;
