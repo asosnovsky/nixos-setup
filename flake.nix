90dcd8d49c7ebaadbf47f073b99bb2d57e60a1f7
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-04-28 14:53:03 -0400

    trying flakes

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
-  inputs.home-manager.url = github:nix-community/home-manager;
-  inputs.fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
+  inputs.home-manager.url = github:nix-community/home-manager/release-23.11;
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
 
@@ -9,7 +8,6 @@
     { self
     , nixpkgs
     , home-manager
-    , fh
     , nixos-hardware
     }@attrs:
     let
@@ -29,7 +27,6 @@
         modules = [
           nixos-hardware.nixosModules.framework-13-7040-amd
           home-manager.nixosModules.default
-          fh.packages.x86_64-linux.default
           (import ./hosts/fwbook.nix {
             user = user;
           })
@@ -58,7 +55,7 @@
           (import ./modules/optional/hyprland.nix {
             user = user;
           })
-          ./modules/optional/gnome.nix
+          # (import ./modules/optional/gnome.nix)
           (import ./modules/optional/home-manager-config.nix {
             user = user;
             hostName = hostName;
