b7c2509817a2778fb54de18fe402b133c51b669b
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-04-28 17:13:04 -0400

    helix

--- modules/optional/home-manager-config.nix
+++ modules/optional/home-manager-config.nix
@@ -63,14 +63,8 @@ in
         plugins = [ "git" "sudo" ];
         theme = "robbyrussell";
       };
-      vscode = {
+      programs.helix = {
         enable = true;
-        extensions = with pkgs.vscode-extensions; [
-          bbenoist.nix
-          ms-python.python
-          ms-azuretools.vscode-docker
-          ms-vscode-remote.remote-ssh
-        ];
       };
     };
   };
