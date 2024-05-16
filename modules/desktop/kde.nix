cd9ba061a4a3a4ea19e643aeaef82949fe39ba09
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-05-16 00:03:13 -0400

    plasma6 disabled some packages

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -2,4 +2,9 @@
 {
   services.desktopManager.plasma6.enable = true;
   services.desktopManager.plasma6.notoPackage = pkgs.fira-code;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    elisa
+    kate
+  ];
 }
