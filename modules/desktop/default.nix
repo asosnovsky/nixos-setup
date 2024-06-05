62e73688802873fca098e28ebc8b7e7d68e7b9f4
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-06-05 10:53:23 -0400

    Updated desktop apps apps

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -5,23 +5,14 @@
 , enableHypr ? false
 , enableWine ? false
 }:
-{ pkgs, ... }:
-{
-  imports = [
-    ./wayland.nix
-  ] ++ (if enableX11 then [
-    ./x11.nix
-  ] else [ ]) ++ (if enableGnome then [
-    ./gnome.nix
-  ] else [ ]) ++ (if enableWine then [
-    ./wine.nix
-  ] else [ ]) ++ (if enableKDE then [
-    ./kde.nix
-  ] else [ ]) ++ (if enableHypr then [
-    (import ./hyprland.nix {
-      user = user;
-    })
-  ] else [ ]);
+{ pkgs, ... }: {
+  imports = [ ./wayland.nix ] ++ (if enableX11 then [ ./x11.nix ] else [ ])
+    ++ (if enableGnome then [ ./gnome.nix ] else [ ])
+    ++ (if enableWine then [ ./wine.nix ] else [ ])
+    ++ (if enableKDE then [ ./kde.nix ] else [ ]) ++ (if enableHypr then
+    [ (import ./hyprland.nix { user = user; }) ]
+  else
+    [ ]);
 
   # Useful Desktop Apps
   programs.kdeconnect.enable = true;
@@ -29,15 +20,21 @@
   users.users.${user.name}.packages = with pkgs; [
     # web
     firefox
+    librewolf
     brave
     # password
-    bitwarden-cli
+    bitwarden-desktop
+    rofi-rbw-wayland
+    rofi-rbw-x11
     # development
     vscode
     # socials
     zoom-us
     betterdiscordctl
     discord
+    signal-desktop
+    whatsapp-for-linux
+    caprine-bin # facebook messenger
     # crypto
     trezor-suite
     trezord
@@ -49,8 +46,5 @@
     vlc-bittorrent
   ];
   # udev rules for crypto wallets
-  services.udev.packages = with pkgs; [
-    ledger-udev-rules
-    trezor-udev-rules
-  ];
+  services.udev.packages = with pkgs; [ ledger-udev-rules trezor-udev-rules ];
 }
