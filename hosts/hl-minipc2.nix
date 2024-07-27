85068ac330b22c5d3ac1303a2d222c9a035a5aed
minipc2 <ariel@sosnovsky.ca>
2024-07-27 12:21:43 -0400

    new rules

--- hosts/hl-minipc2.nix
+++ hosts/hl-minipc2.nix
@@ -6,10 +6,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # Special udev rule (coral tpu)
-  services.udev.extraRules = ''
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1a6e", ATTRS{idProduct}=="089a", MODE="0664", TAG+="uaccess", SYMLINK+="coraltpu"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="9302", MODE="0664", TAG+="uaccess", SYMLINK+="coraltpu"
-  '';
+  # Special udev rule for google's coral tpu
+  homelab.udevrules.coraltpu.enable = true;
 }
 
