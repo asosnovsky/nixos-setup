b313d7a4f808a5ec38ae0ebf427b04148857be2f
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-07-31 12:07:00 -0400

    Updates to config for terra

--- hosts/hl-terra1.nix
+++ hosts/hl-terra1.nix
@@ -6,5 +6,22 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.extraModulePackages = with pkgs.linuxPackages; [ it87 ];
+  boot.kernelModules = ["coretemp" "it87" "drivetemp"];
+systemd.services.fancontrol = {
+  enable = true;
+  description = "Fan control";
+  wantedBy = ["multi-user.target" "graphical.target" "rescue.target"];
+
+  unitConfig = {
+    Type = "simple";
+  };
+
+  serviceConfig = {
+    ExecStart = "${pkgs.lm_sensors}/bin/fancontrol";
+    Restart = "always";
+  };
+};
+
 }
 
