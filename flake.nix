699d47f4bddbb8886f757e6e558f745fa246bd2e
Ari Sosnovsky <ariel@sosnovsky.ca>
2024-08-02 13:45:23 -0400

    Less overhead for new homelab devices

--- flake.nix
+++ flake.nix
@@ -105,57 +105,27 @@
         };
       };
       makeHLService = lib.makeNixOsModuleMaker hlCommonSettings;
+      homelabMachines = [
+        "hl-bigbox1"
+        "hl-minipc1"
+        "hl-minipc2"
+        "hl-terra1"
+      ];
+      homeLabModules = builtins.map
+        (name: {
+          name = name;
+          value = makeHLService {
+            hostName = name;
+            configuration = (import (./hosts + "/${name}.nix") { user = user; });
+            systemStateVersion = "24.05";
+          };
+        })
+        homelabMachines;
     in
     {
       lib = lib;
       formatter =
         eachSystem (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
-
-      # NIXOS Framework Setups
-      # -------------
-      nixosConfigurations."fwbook" = lib.makeNixOsModule {
-        system = "x86_64-linux";
-        user = user;
-        systemStateVersion = "23.11";
-        hostName = "fwbook";
-        localNixCaches = localNixCaches;
-        localDockerRegistries = localDockerRegistries;
-        home-manager = {
-          enable = true;
-          version = homeManagerVersion;
-          enableDevelopmentKit = true;
-        };
-        desktop = {
-          enable = true;
-          user = user;
-          enableKDE = true;
-          enableHypr = false;
-          enableX11 = true;
-          enableWine = true;
-        };
-        os = {
-          enable = true;
-          firewall = { enable = false; };
-          enableFonts = true;
-          enableNetworking = true;
-          enableSSH = false;
-          hardware = { enable = true; };
-          containers = {
-            localDockerRegistries = localDockerRegistries;
-            runtime = "docker";
-          };
-          enablePrometheusExporters = true;
-        };
-        configuration = { ... }: {
-          imports = [
-            nixos-hardware.nixosModules.framework-13-7040-amd
-            (import ./hosts/fwbook.nix {
-              user = user;
-              unstable = unstablePkgs.x86_64-linux;
-            })
-          ];
-        };
-      };
 
       # MacBooks Setups
       # -------------
@@ -174,46 +144,77 @@
           (import ./hosts/asosnovsky-mac.nix { user = sumoUser; });
       };
 
-      # NIXOS Homelab - BIGBOX1
-      # -------------
-      nixosConfigurations."hl-bigbox1" = makeHLService {
-        hostName = "hl-bigbox1";
-        configuration = (import ./hosts/hl-bigbox1.nix { user = user; });
-        systemStateVersion = "24.05";
-      };
-      # unstableLib.makeNixOsModule (hlCommonSettings // {
-      #   systemStateVersion = "24.05";
-      #   hostName = "hl-bigbox1";
-      #   home-manager = {
-      #     enable = true;
-      #     enableDevelopmentKit = false;
-      #     version = "24.11";
+
+      nixosConfigurations = {
+        # NIXOS Framework Setups
+        # -------------
+        fwbook = lib.makeNixOsModule {
+          system = "x86_64-linux";
+          user = user;
+          systemStateVersion = "23.11";
+          hostName = "fwbook";
+          localNixCaches = localNixCaches;
+          localDockerRegistries = localDockerRegistries;
+          home-manager = {
+            enable = true;
+            version = homeManagerVersion;
+            enableDevelopmentKit = true;
+          };
+          desktop = {
+            enable = true;
+            user = user;
+            enableKDE = true;
+            enableHypr = false;
+            enableX11 = true;
+            enableWine = true;
+          };
+          os = {
+            enable = true;
+            firewall = { enable = false; };
+            enableFonts = true;
+            enableNetworking = true;
+            enableSSH = false;
+            hardware = { enable = true; };
+            containers = {
+              localDockerRegistries = localDockerRegistries;
+              runtime = "docker";
+            };
+            enablePrometheusExporters = true;
+          };
+          configuration = { ... }: {
+            imports = [
+              nixos-hardware.nixosModules.framework-13-7040-amd
+              (import ./hosts/fwbook.nix {
+                user = user;
+                unstable = unstablePkgs.x86_64-linux;
+              })
+            ];
+          };
+        };
+      } // builtins.listToAttrs homeLabModules;
+
+      #   # Homelab
+      #   # -------------
+      #   hl-bigbox1 = makeHLService {
+      #     hostName = "hl-bigbox1";
+      #     configuration = (import ./hosts/hl-bigbox1.nix { user = user; });
+      #     systemStateVersion = "24.05";
       #   };
-      #   configuration = (import ./hosts/hl-bigbox1.nix { user = user; });
-      # });
-
-      # NIXOS Homelab - minipc1
-      # -------------
-      nixosConfigurations."hl-minipc1" = makeHLService {
-        hostName = "hl-minipc1";
-        configuration = (import ./hosts/hl-minipc1.nix { user = user; });
-        systemStateVersion = "23.11";
-      };
-
-      # NIXOS Homelab - minipc2
-      # -------------
-      nixosConfigurations."hl-minipc2" = makeHLService {
-        hostName = "hl-minipc2";
-        configuration = (import ./hosts/hl-minipc2.nix { user = user; });
-        systemStateVersion = "24.05";
-      };
-
-      # NIXOS Homelab - terramaster 1
-      # -------------
-      nixosConfigurations."hl-terra1" = makeHLService {
-        hostName = "hl-terra1";
-        configuration = (import ./hosts/hl-terra1.nix { user = user; });
-        systemStateVersion = "24.05";
-      };
+      #   hl-minipc1 = makeHLService {
+      #     hostName = "hl-minipc1";
+      #     configuration = (import ./hosts/hl-minipc1.nix { user = user; });
+      #     systemStateVersion = "23.11";
+      #   };
+      #   hl-minipc2 = makeHLService {
+      #     hostName = "hl-minipc2";
+      #     configuration = (import ./hosts/hl-minipc2.nix { user = user; });
+      #     systemStateVersion = "24.05";
+      #   };
+      #   hl-terra1 = makeHLService {
+      #     hostName = "hl-terra1";
+      #     configuration = (import ./hosts/hl-terra1.nix { user = user; });
+      #     systemStateVersion = "24.05";
+      #   };
+      # };
     };
 }
