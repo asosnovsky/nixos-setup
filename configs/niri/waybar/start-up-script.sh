fd89da4defc8b502712325e75132fdeb6199d89f
Ari Sosnovsky <ariel@sosnovsky.ca>
2025-09-21 13:52:49 -0400

    clean up

--- configs/niri/waybar/start-up-script.sh
+++ configs/niri/waybar/start-up-script.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 killall -q waybar
+sleep 1
 
 waybar --config /home/ari/nixos-setup/configs/niri/waybar/bottom-bar.jsonc --style /home/ari/nixos-setup/configs/niri/waybar/bottom-bar.css &
 waybar --config /home/ari/nixos-setup/configs/niri/waybar/top-bar.jsonc --style /home/ari/nixos-setup/configs/niri/waybar/top-bar.css &
 waypaper --restore
+
+killall -q hypridle
+hypridle &
+
+killall -q swayosd-server
+swayosd-server
+
+notify-send "Niri Startup Script" "Completed 🎉"
