2b4ad5c063f928f7478a7f723f820ebf2cbf6c20
Ari Sosnovsky <ariel@sosnovsky.ca>
2025-11-13 10:41:42 -0500

    always on

--- modules/nixos/desktop/stylix/default.nix
+++ modules/nixos/desktop/stylix/default.nix
@@ -5,11 +5,13 @@ in
 {
   options = {
     skyg.nixos.desktop.stylix = {
-      enable = lib.mkEnableOption
-        "Enable Stylix";
+      enable = (lib.mkEnableOption
+      "Enable Stylix") // {
+      default = true;
+      };
     };
   };
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf (cfg.enable && config.skyg.nixos.desktop.enable) {
     stylix.enable = true;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     stylix.polarity = "dark";
