7b46766d0c73f8a3195578a8fcb6943f410d3572
Ari Sosnovsky <ariel@sosnovsky.ca>
2026-09-16 16:19:16 -0400

    autostart restart

--- configs/fwbook/hypr/conf/autostart.lua
+++ configs/fwbook/hypr/conf/autostart.lua
@@ -1,8 +1,10 @@
 -- =========================
 -- Autostart
 -- =========================
-hl.on("hyprland.start", function()
+local function autostart()
     hl.exec_cmd("setpriv --ambient-caps -all -- noctalia")
     hl.exec_cmd("setpriv --ambient-caps -all -- qs -c overview")
     hl.exec_cmd("setpriv --ambient-caps -all -- hypridle")
-end)
+end
+hl.on("hyprland.start", autostart)
+hl.bind("SUPER + SHIFT + A", autostart)
